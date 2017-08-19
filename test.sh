34f8f97a19d1a764bcd5d013af2271fac65fa0b5
Ryan <ryannitz@amazon.com>
2017-08-18 20:59:33 -0400

    Layout

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-
-curl -H "Content-Type: text/plain" -X POST -d 'public class Test { public static void main(final String [] pArgs) { System.out.println("Hello World!"); } }' http://localhost:8080
-
-
+curl \
+  -H "Content-Type: text/plain" \
+  -X POST \
+  -d 'public class Test { public static void main(final String [] pArgs) { System.out.println("Hello World!"); } }' \
+  http://localhost:8080
